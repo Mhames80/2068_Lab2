745b8d155b406afc5fdf74722b05f1402cd4249f
Matt <mhames80@gmail.com>
2020-06-04 18:37:58 -0400

    done and done

--- 2068_lab2/routes/index.js
+++ 2068_lab2/routes/index.js
@@ -4,7 +4,40 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    var result;
+
+    function calculate(method, x, y) {
+        var xNumber = parseInt(x);
+        var yNumber = parseInt(y);
+        
+        if (method === "add") {
+            result = x + " + " + y + " = " + (xNumber + yNumber);
+            console.log(result);
+        }
+        else if (method === "subtract") {
+            result = x + " - " + y + " = " + (xNumber - yNumber)
+            console.log(result);
+        }
+        else if (method === "multiply") {
+            result = x + " x " + y + " = " + (xNumber * yNumber)
+            console.log(result);
+        }
+        else if (method === "divide") {
+            result = x + " / " + y + " = " + (xNumber / yNumber)
+            console.log(result);
+        }
+        else {
+            result = "Error"
+            console.log(result);
+        }
+    }
+
+    if (Object.keys(req.query).length > 0) {
+        calculate(req.query.method, req.query.x, req.query.y);
+    }
+
+
+    res.render('index', { title: 'Calculation', calculate: result });
 });
 
 module.exports = router;
